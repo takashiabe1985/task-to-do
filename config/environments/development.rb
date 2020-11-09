661f2dcb056bba16ee5b7e3a1c6aea640870e016
dani.camp <bboydanee@gmail.com>
2020-11-09 13:05:59 +0000

    for heroku add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-
+　config.hosts << "abetask-to-do.herokuapp.com"
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
